bb9a77821e6473b010520154bb961efed6526844
chidi150c <chidi150c@gmail.com>
2023-08-08 15:39:33 +0100

    Update config.go

--- config/config.go
+++ config/config.go
@@ -21,8 +21,8 @@ func NewExchangesConfig()map[string]*ExchConfig{
 			Exch: Api{
 				BaseURL: "https://api.binance.com",
 				ApiVersion: "api/v3",
-				ApiKey: "1CHA2mXswJdHjfossO43t4WRa82HPzFaeZOt2entAgajkAYIUaf55f7CepLt58YK", // Replace with your Binance API key
-				SecretKey: "0DiulGqjlOuQlQHcVQYLYjKfpkq6Qs2rNxNVHHTtF1s2uy7n5clugQRxTXltjqFj", // Replace with your Binance API secret key
+				ApiKey: "", // Replace with your Binance API key
+				SecretKey: "", // Replace with your Binance API secret key
 				Symbol: "BTCUSDT",
 				Interval: "30m",
 			},
@@ -32,8 +32,8 @@ func NewExchangesConfig()map[string]*ExchConfig{
 			Exch: Api{
 				BaseURL: "https://api.hitbtc.com",
 				ApiVersion: "api/3",
-				ApiKey: "055a27622c79b892c197cdbf963ec641", // Replace with your Binance API key
-				SecretKey: "ca2cc7c96ea711765c0f248c678a1863", // Replace with your Binance API secret key
+				ApiKey: "", // Replace with your Binance API key
+				SecretKey: "", // Replace with your Binance API secret key
 				Symbol: "BTCUSDT",
 				Interval: "m30",
 			},
