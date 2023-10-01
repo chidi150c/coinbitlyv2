f650221865992d74e610e8b50041f8ce8b1e4f4f
chidi150c <chidi150c@gmail.com>
2023-10-02 00:21:29 +0100

    i will make it in this USA land

--- strategies/strategies.go
+++ strategies/strategies.go
@@ -905,10 +905,10 @@ func (ts *TradingSystem) RiskManagement(md *model.AppData) string {
 // Bands. It determines the buy and sell signals based on various strategy rules.
 func (ts *TradingSystem) TechnicalAnalysis(md *model.AppData, Action string) (buySignal, sellSignal bool) {
 	// Calculate moving averages (MA) using historical data.
-	// longEMA, period3EMA, err := CandleExponentialMovingAverage(ts.ClosingPrices, md.LongPeriod, 3)
-	// shortEMA, err := CandleExponentialMovingAverageV1(period3EMA, md.ShortPeriod)
-	period3EMA, err := CandleExponentialMovingAverageV1(ts.ClosingPrices, 3)
-	longEMA, shortEMA, err := CandleExponentialMovingAverage(period3EMA, md.LongPeriod, md.ShortPeriod)
+	longEMA, period3EMA, err := CandleExponentialMovingAverage(ts.ClosingPrices, md.LongPeriod, 3)
+	shortEMA, err := CandleExponentialMovingAverageV1(period3EMA, md.ShortPeriod)
+	// period3EMA, err := CandleExponentialMovingAverageV1(ts.ClosingPrices, 3)
+	// longEMA, shortEMA, err := CandleExponentialMovingAverage(period3EMA, md.LongPeriod, md.ShortPeriod)
 	if err != nil {
 		// log.Printf("Error: in TechnicalAnalysis Unable to get EMA: %v", err)
 		md.LongEMA, md.ShortEMA = 0.0, 0.0
