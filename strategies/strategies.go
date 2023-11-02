5b636052e1f809ce45ea69d5621a0829c9cfead4
chidi150c <chidi150c@gmail.com>
2023-11-02 00:02:07 -0500

    more profit making worked2

--- strategies/strategies.go
+++ strategies/strategies.go
@@ -101,7 +101,7 @@ func NewTradingSystem(BaseCurrency string, liveTrading bool, loadExchFrom, loadD
 			ts = &TradingSystem{}
 			ts.RiskFactor = 2.0
 			ts.CommissionPercentage = 0.00075
-			ts.RiskProfitLossPercentage = 0.0008
+			ts.RiskProfitLossPercentage = 0.001
 			ts.EnableStoploss = true
 			ts.StopLossRecover = append(ts.StopLossRecover, math.MaxFloat64)
 			ts.MaxDataSize = 500
@@ -118,7 +118,7 @@ func NewTradingSystem(BaseCurrency string, liveTrading bool, loadExchFrom, loadD
 			ts = &TradingSystem{}
 			ts.RiskFactor = 2.0
 			ts.CommissionPercentage = 0.00075
-			ts.RiskProfitLossPercentage = 0.0008
+			ts.RiskProfitLossPercentage = 0.001
 			ts.EnableStoploss = true
 			ts.StopLossRecover = append(ts.StopLossRecover, math.MaxFloat64)
 			ts.MaxDataSize = 500
@@ -126,7 +126,7 @@ func NewTradingSystem(BaseCurrency string, liveTrading bool, loadExchFrom, loadD
 		} else {
 			loadDataFrom = "DataBase"
 			// ts.InitialCapital = 54.038193 + 26.47
-			// ts.RiskProfitLossPercentage = 0.0008
+			// ts.RiskProfitLossPercentage = 0.001
 
 			// ts.ClosingPrices = append(ts.ClosingPrices, ts.CurrentPrice)
 			// ts.Timestamps = append(ts.Timestamps, time.Now().Unix())
@@ -302,8 +302,8 @@ func (ts *TradingSystem) NewAppData(loadExchFrom string) *model.AppData {
 		}else{
 			// md.ShortPeriod = 15 //10 Define moving average short period for the strategy.
 			// md.LongPeriod = 55  //30 Define moving average long period for the strategy.
-			// md.TargetProfit = (54.038193 + 26.47) * 0.0008
-			// md.TargetStopLoss = (54.038193 + 26.47) * 0.0008
+			md.TargetProfit = (54.038193 + 26.47) * 0.001
+			md.TargetStopLoss = (54.038193 + 26.47) * 0.001
 		}
 	}
 	fmt.Println("MD = ", md)
