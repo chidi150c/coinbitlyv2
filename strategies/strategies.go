05bd5d4dcc42c917a1b20ccffd8c26a9891d1204
chidi150c <chidi150c@gmail.com>
2023-11-20 14:04:12 -0600

    Epoch Adjustment to speed up3

--- strategies/strategies.go
+++ strategies/strategies.go
@@ -125,9 +125,9 @@ func NewTradingSystem(BaseCurrency string, liveTrading bool, loadExchFrom, loadD
 			ts.BaseCurrency = BaseCurrency
 		} else {
 			loadDataFrom = "DataBase"
-			// ts.InitialCapital = 54.038193 + 26.47 + 54.2
+			ts.InitialCapital = 54.038193 + 26.47 + 54.2 + 86.5
 			//ts.RiskProfitLossPercentage = 0.0008
-			ts.EpochTime = time.Second * 10
+			//ts.EpochTime = time.Second * 10
 			// ts.ClosingPrices = append(ts.ClosingPrices, ts.CurrentPrice)
 			// ts.Timestamps = append(ts.Timestamps, time.Now().Unix())
 	
