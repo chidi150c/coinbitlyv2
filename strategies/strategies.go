768a458494642141ed80d0ebc10150c50f53417c
chidi150c <chidi150c@gmail.com>
2024-01-16 19:40:00 -0600

    New Structure and AI int4

--- strategies/strategies.go
+++ strategies/strategies.go
@@ -1154,7 +1154,8 @@ func (ts *TradingSystem) RiskManagement(dp *model.DataPoint) {
 }
 func (ts *TradingSystem) AIAnalysis(dp *model.DataPoint, Action string) (buySignal, sellSignal bool) {
 	// Create an instance of DataPointRequest and populate it with data
-	if strings.Contains(ts.RDBServices.loadExchFrom, "TestnetWithOutAI") {
+	if strings.Contains(ts.RDBServices.loadExchFrom, "TestnetWithOutAI") || true{
+		ts.Log.Printf("ATAnalysis reached with: %v", dp)
 		return true, true
 	}
 	// Convert dataPoint to JSON
@@ -1186,7 +1187,7 @@ func (ts *TradingSystem) AIAnalysis(dp *model.DataPoint, Action string) (buySign
 	var prediction model.PredictionResponse
 	err = json.Unmarshal(responseData, &prediction)
 	if err != nil {
-		ts.Log.Printf("Error occurred during unmarshaling of AI Model!!!. Error: %s jsonResp = %v jsonRequest = %v", err.Error(), responseData, jsonData)
+		ts.Log.Printf("Error occurred during unmarshaling of AI Model!!!. Error: %s", err.Error())
 		return true, true
 	}
 
